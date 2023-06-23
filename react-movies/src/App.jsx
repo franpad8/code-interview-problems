555fd8edc0fa32ef92ea7b34fdf41b96b17c6721
Franklin Padilla <fpadilla@buk.cl>
2023-06-23 12:30:17 -0400

    Add mocked results and show it to user

--- react-movies/src/App.jsx
+++ react-movies/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { withResults } from './mocks/movies'
 
 function App () {
   return (
@@ -12,6 +13,22 @@ function App () {
           <button type='submit' className='button'>Search</button>
         </form>
       </header>
+
+      <main>
+        <ul className='results'>
+          {
+            withResults.Search.map((movie) => (
+              <li key={movie.imdbID} className='movie'>
+                <div className='movie-info'>
+                  <h3>{movie.Title}</h3>
+                  <span>{movie.Year}</span>
+                </div>
+                <img src={movie.Poster} alt={movie.Title} />
+              </li>
+            ))
+          }
+        </ul>
+      </main>
     </div>
   )
 }
