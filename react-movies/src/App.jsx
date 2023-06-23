41505755d5acd429fff03e152938b8cb197d2b5a
Franklin Padilla <fpadilla@buk.cl>
2023-06-23 12:09:09 -0400

    react-movies: add search input (without logic)

--- react-movies/src/App.jsx
+++ react-movies/src/App.jsx
@@ -3,9 +3,15 @@ import './App.css'
 function App () {
   return (
     <div id='page'>
-      <h1>
-        Movie Searcher
-      </h1>
+      <header>
+        <h1>
+          Movie Searcher
+        </h1>
+        <form className='form'>
+          <input type='text' className='search' placeholder='Avengers, Matrix, Batman...' />
+          <button type='submit' className='button'>Search</button>
+        </form>
+      </header>
     </div>
   )
 }
