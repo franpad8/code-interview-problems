3c6b2a87d82332b105b439e1db6ad64297a022e3
Franklin Padilla <fpadilla@buk.cl>
2023-06-23 12:44:18 -0400

    react-movies: handle no results case

--- react-movies/src/App.jsx
+++ react-movies/src/App.jsx
@@ -1,33 +1,60 @@
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
-import { withResults } from './mocks/movies'
+import { withResults, withoutResults } from './mocks/movies'
 
 function App () {
+  const [movies, setMovies] = useState([])
+  const isFirstRender = useRef(true)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    isFirstRender.current = false
+    setMovies(withoutResults.Search || [])
+  }
+
+  const renderListOfMovies = () => {
+    if (isFirstRender.current) return
+
+    return (
+      <>
+        {
+          movies.length
+            ? (
+              <ul className='results'>
+                {
+                  movies.map((movie) => (
+                    <li key={movie.imdbID} className='movie'>
+                      <div className='movie-info'>
+                        <h3>{movie.Title}</h3>
+                        <span>{movie.Year}</span>
+                      </div>
+                      <img src={movie.Poster} alt={movie.Title} />
+                    </li>
+                  ))
+                }
+              </ul>
+              )
+            : (
+              <p>No se encontraron resultados</p>
+              )
+        }
+      </>
+    )
+  }
   return (
     <div id='page'>
       <header>
         <h1>
           Movie Searcher
         </h1>
-        <form className='form'>
-          <input type='text' className='search' placeholder='Avengers, Matrix, Batman...' />
+        <form onSubmit={handleSubmit} className='form'>
+          <input type='text' name='search' placeholder='Avengers, Matrix, Batman...' />
           <button type='submit' className='button'>Search</button>
         </form>
       </header>
 
       <main>
-        <ul className='results'>
-          {
-            withResults.Search.map((movie) => (
-              <li key={movie.imdbID} className='movie'>
-                <div className='movie-info'>
-                  <h3>{movie.Title}</h3>
-                  <span>{movie.Year}</span>
-                </div>
-                <img src={movie.Poster} alt={movie.Title} />
-              </li>
-            ))
-          }
-        </ul>
+        {renderListOfMovies()}
       </main>
     </div>
   )
