16bb4c9de519958b8abe05584d0aac6dc31354ed
Franklin Padilla <fpadilla@buk.cl>
2023-06-24 01:19:31 -0400

    react-movies: avoid redoing the same search

--- react-movies/src/hooks/useMovies.js
+++ react-movies/src/hooks/useMovies.js
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { searchMovies } from '../services/movies'
 
 export function useMovies () {
   const [movies, setMovies] = useState(null)
   const [loading, setLoading] = useState(false)
+  const previousSearch = useRef('')
 
   function getMovies (search) {
+    if (previousSearch.current === search) return
+    previousSearch.current = search
     setLoading(true)
     searchMovies(search)
       .then(returnedMovies => {
